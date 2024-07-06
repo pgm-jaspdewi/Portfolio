a88e12247760571a9cc2b1ce8244e2a1c8c73641
Jasper De Wilde <jasper.dewilde1@student.arteveldehs.be>
2024-07-06 18:08:16 +0200

    Bento now fully responsive

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,12 +1,59 @@
-import React from 'react'
+'use client';
+
+import React, { useEffect, useState } from 'react'
 import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
 import { gridItems } from '@/data'
 
+type GridItem = {
+  id: number;
+  title: string;
+  description: string;
+  className: string;
+  imgClassName: string;
+  titleClassName: string;
+  img: string;
+  spareImg: string;
+};
+
 const Grid = () => {
+
+  const getViewportSize = () => {
+    if (typeof window === 'undefined') return 'lg';
+    const width = window.innerWidth;
+    if (width >= 1024) return 'lg';
+    if (width >= 768) return 'md';
+    return 'sm';
+  };
+
+  const reorderItems = (items: GridItem[], viewportSize: string): GridItem[] => {
+    switch (viewportSize) {
+      case 'lg':
+        return [...items];
+      case 'md':
+        return [...items];
+      default:
+        return [items[0], items[1], items[3], items[2]];
+    }
+  };
+
+  const [viewportSize, setViewportSize] = useState<string>(getViewportSize());
+  const [orderedItems, setOrderedItems] = useState<GridItem[]>(reorderItems(gridItems, viewportSize));
+
+  useEffect(() => {
+    const handleResize = () => {
+      const newViewportSize = getViewportSize();
+      setViewportSize(newViewportSize);
+      setOrderedItems(reorderItems(gridItems, newViewportSize));
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <section id='about'>
       <BentoGrid>
-        {gridItems.map
+        {orderedItems.map
           (({ id, title, description, className, img, imgClassName, titleClassName, spareImg }) => (
             <BentoGridItem
               id={id}
