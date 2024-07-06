0de346bdc0aaf3854318636ce4bf6014fee28892
Jasper De Wilde <jasper.dewilde1@student.arteveldehs.be>
2024-07-06 10:24:09 +0200

    Updated tailwind.config.ts & globals.css. Working on Hero.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto px-5 sm:px-10">
       <div className="max-w-7xl w-full">
-        <h1>Welcome to Jasper&apos;s Portfolio</h1>
+        <Hero />
       </div>
     </main>
   );
