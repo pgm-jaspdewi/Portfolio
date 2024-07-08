826daa62663ffb2a0684fff97b19ba1d0bba75b7
Jasper De Wilde <jasper.dewilde1@student.arteveldehs.be>
2024-07-08 16:41:40 +0200

    Portfolio design finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -6,13 +8,15 @@ import { navItems } from "@/data";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto px-5 sm:px-10">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto px-5 sm:px-10 overflow-clip">
       <div className="max-w-7xl w-full">
         <FloatingNav
           navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
+        <Experience />
+        <Footer />
       </div>
     </main>
   );
