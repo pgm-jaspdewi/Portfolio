ef992dab399eee9fabb42285ecebba0578922e6b
Jasper De Wilde <jasper.dewilde1@student.arteveldehs.be>
2024-07-11 11:37:20 +0200

    Slightly changed wording

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,7 +14,6 @@ const Hero = () => {
       </div>
 
       <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
-        {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
 
@@ -30,7 +29,7 @@ const Hero = () => {
           />
 
           <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
-            A web developer based in East Flanders.
+            A web developer based in Dendermonde, East Flanders.
           </p>
 
           <a href="#projects">
