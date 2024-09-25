f9873bb668b88d6e295ec03b241f0b24756606f6
Jasper De Wilde <jasper.dewilde1@student.arteveldehs.be>
2024-09-25 15:27:37 +0200

    Update RecentProjects.tsx

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -10,7 +10,7 @@ const RecentProjects = (props: Props) => {
   return (
     <div className='py-20' id='projects'>
       <h1 className='heading'>
-        A small selection of {''}
+        A selection of {''}
         <span className='text-purple'>recent projects</span>
       </h1>
       <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10'>
